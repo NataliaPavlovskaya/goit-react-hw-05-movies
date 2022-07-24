65c10e5b7e5e4c7a04849ba5ff4f14174e617b17
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-07-24 20:43:29 +0300

    navigation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { lazy, useEffect, Suspense } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import AppBar from './components/AppBar';
+// import HomePage from './pages/HomePage/HomePage'
+
+
+export default function App() {
+  return (
+    <>
+      <AppBar />
+     </>
+  );
+}
--- src/services/api.js
+++ src/services/api.js
@@ -3,90 +3,10 @@ import axios from 'axios';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'd2088e493093cc1b7e7b50d5b735bf4d';
 
-axios.defaults.baseURL = BASE_URL;
-axios.defaults.params = {
-  api_key: API_KEY,
-  language: 'en-US',
-};
 
-async function getPopularMovies(page) {
-  try {
-    const config = {
-      url: `trending/movie/day`,
-      params: {
-        page,
-      },
-    };
-    const { data } = await axios(config, page);
-    return data;
-  } catch (error) {
-    console.log('error', { error });
-    return null;
-  }
+export async function requestTrendingMovies(page = 1) {
+  const { data } = await axios.get(
+    `${BASE_URL}/trending/all/day?api_key=${API_KEY}&page=${page}&language=ru-RU&include_image_language=en`,
+  );
+  return data;
 }
-
-async function getMoviesByKeyWord(query, page) {
-  try {
-    const config = {
-      url: `search/movie`,
-      params: {
-        query,
-        page,
-      },
-    };
-    const { data } = await axios(config, query, page);
-    return data;
-  } catch (error) {
-    console.log('error', { error });
-    return null;
-  }
-}
-
-async function getMovieById(id) {
-  try {
-    const config = {
-      url: `movie/${id}`,
-    };
-    const { data } = await axios(config, id);
-    return data;
-  } catch (error) {
-    console.log('error', { error });
-    return null;
-  }
-}
-
-async function getCastInfo(id) {
-  try {
-    const config = {
-      url: `movie/${id}/credits`,
-    };
-    const { data } = await axios(config, id);
-    return data.cast;
-  } catch (error) {
-    console.log('error', { error });
-    return null;
-  }
-}
-
-async function getMovieReview(id) {
-  try {
-    const config = {
-      url: `movie/${id}/reviews`,
-    };
-    const { data } = await axios(config, id);
-    return data.results;
-  } catch (error) {
-    console.log('error', { error });
-    return null;
-  }
-}
-
-const apiService = {
-  getPopularMovies,
-  getMoviesByKeyWord,
-  getMovieById,
-  getCastInfo,
-  getMovieReview,
-};
-
-export default apiService;
